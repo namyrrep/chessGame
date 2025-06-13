d3cbcc563f9160d2a7b74eb20adb137df958efd1
namyrrep <namyrrep@vt.edu>
2025-06-12 22:04:13 -0400

    Update Board.java
    Made Check Method
    Named Kings

--- board.java
+++ board.java
@@ -26,7 +26,8 @@ public class Board {
         Gameboard[0][1] = new Knight(Player.PlayerColor.WHITE, 1, 0);
         Gameboard[0][2] = new Bishop(Player.PlayerColor.WHITE, 2, 0);
         Gameboard[0][3] = new Queen(Player.PlayerColor.WHITE, 3, 0);
-        Gameboard[0][4] = new King(Player.PlayerColor.WHITE, 4, 0);
+        King whiteKing = new King(Player.PlayerColor.WHITE, 4, 0);
+        Gameboard[0][4] = whiteKing;
         Gameboard[0][5] = new Bishop(Player.PlayerColor.WHITE, 5, 0);
         Gameboard[0][6] = new Knight(Player.PlayerColor.WHITE, 6, 0);
         Gameboard[0][7] = new Rook(Player.PlayerColor.WHITE, 7, 0);
@@ -35,7 +36,8 @@ public class Board {
         Gameboard[7][1] = new Knight(Player.PlayerColor.BLACK, 1, 7);
         Gameboard[7][2] = new Bishop(Player.PlayerColor.BLACK, 2, 7);
         Gameboard[7][3] = new Queen(Player.PlayerColor.BLACK, 3, 7);
-        Gameboard[7][4] = new King(Player.PlayerColor.BLACK, 4, 7);
+        King blackKing = new King(Player.PlayerColor.BLACK, 4, 7);
+        Gameboard[7][4] = blackKing;
         Gameboard[7][5] = new Bishop(Player.PlayerColor.BLACK, 5, 7);
         Gameboard[7][6] = new Knight(Player.PlayerColor.BLACK, 6, 7);
         Gameboard[7][7] = new Rook(Player.PlayerColor.BLACK, 7, 7);
@@ -77,5 +79,51 @@ public class Board {
     }
     public boolean isSquareOccupied(int row, int col) {
         return isInBounds(row, col) && Gameboard[row][col] != null; // Check if the square is occupied by a piece
+    }
+
+    //Before every move, is made this will check the board to see if the move is valid
+    //It will go through the board and see if either player is in check
+    public boolean isChecked(Player.PlayerColor color) {
+        // Check if the specified player is in check
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Piece piece = Gameboard[i][j];
+                if (piece != null && piece.getColor() == color) {
+                    // Check if the piece can be attacked by any opponent's piece
+                    if (isUnderAttack(piece)) {
+                        return true; // Player is in check
+                    }
+                }
+            }
+        }
+        return false; // Player is not in check
+    }
+
+    /**
+     * Checks if the given piece is under attack by any opponent's piece.
+     * This method finds all pieces of the opposite color and checks if any of them
+     * can move to the position of the given piece (targetRow, targetCol).
+     * If any opponent piece has a valid move to this square, the piece is considered under attack.
+     *
+     * @param piece The piece to check for threats.
+     * @return true if the piece is under attack, false otherwise.
+     */
+    private boolean isUnderAttack(Piece piece) {
+        Player.PlayerColor opponentColor = (piece.getColor() == Player.PlayerColor.WHITE)
+                ? Player.PlayerColor.BLACK : Player.PlayerColor.WHITE;
+        int targetRow = piece.getRow();
+        int targetCol = piece.getCol();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Piece attacker = Gameboard[i][j];
+                // Check if this is an opponent's piece and if it can move to the target square
+                if (attacker != null && attacker.getColor() == opponentColor) {
+                    if (attacker.isValidMove(targetRow, targetCol, this)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
