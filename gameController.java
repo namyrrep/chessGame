aa6fc21bfc78a0e8ac324a5e822da2216d9a8746
edwinb1067 <edwinb1067@vt.edu>
2025-07-06 16:51:36 -0400

    Update gameController.java

--- gameController.java
+++ gameController.java
@@ -5,3 +5,10 @@
  * Detects check, checkmate, stalemate, and draw.
  * Handles move validation, special moves (castling, en passant, promotion), and move history.
  */
+public class GameController
+{
+    public GameController()
+    {
+        
+    }
+}
