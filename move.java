2fdb182ebe7e59e4624b663bc62830d9369dbde8
edwinb1067 <edwinb1067@vt.edu>
2025-07-08 14:29:42 -0400

    Started the Move class

--- move.java
+++ move.java
@@ -4,3 +4,30 @@
  * Represents a single chess move (from, to, piece moved, captured piece, etc.).
  * Used for move history, undo/redo functionality, and move validation.
  */
+public class Move
+{
+    //All initialized fields
+    Piece piece;
+    int row;
+    int col;
+
+    //Sets the piece being moved (holds new position) and stores old position
+    public Move(Piece movedPiece, int previousRow, int previousCol)
+    {
+        piece = movedPiece;
+        row = previousRow;
+        col = previousCol;
+    }
+
+    //This is the getter method for row
+    public int getRow()
+    {
+        return row;
+    }
+
+    //This is the getter method for col
+    public int getCol()
+    {
+        return col;
+    }
+}
